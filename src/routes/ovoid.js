7cf2e7115cf46bec544b16dd518167a9535e0f81
user <user@DESKTOP-VMMC1CR>
2020-09-16 14:02:25 +0700

    update ovoid

--- src/routes/ovoid.js
+++ src/routes/ovoid.js
@@ -1,27 +1,47 @@
 const OVOID = require('ovoid');
 var express = require('express');
-const { response } = require('express');
 var router = express.Router();
 let ovoid = new OVOID();
-
-const login = async (nomor) => {
-    await ovoid.login2FA(nomor)
-        .then((json) => {
-            json.json();
-        })
-        .then((response) => {
-            return response;
-        })
-        .catch((err) => {
-            return err;
-        });
+let state = {
+    refId: '',
+    accessToken: {}
 }
 
-router.route('/').post(async function(req, res) {
-    let nomor = req.body.nomor;
-    let refId = login(nomor);
-    
-    res.send(refId);
+router.route('/').post(async function(req, res, next) {
+    try {
+        let nomor = req.body.nomor;
+        let refId = await ovoid.login2FA(nomor);
+        state.refId = refId;
+        
+        res.send(refId);
+    } catch (err) {
+        next(err)
+    }
+});
+
+router.route('/otp').post(async function(req, res, next) {
+    try {
+        let otp = req.body.otp;
+        let nomor = req.body.nomor;
+        let accessToken = await ovoid.login2FAVerify(state.refId, otp, nomor);
+        state.accessToken = accessToken;
+
+        res.send(accessToken);
+    } catch (err) {
+        next(err)
+    }
+});
+
+router.route('/pin').post(async function(req, res, next) {
+    try {
+        let pin = req.body.pin;
+        let authToken = await ovoid.loginSecurityCode(pin, state.accessToken.updateAccessToken);
+        ovoid = new OVOID(authToken.token);
+
+        res.send(authToken);
+    } catch (err) {
+        next(err)
+    }
 });
 
 module.exports = router;
